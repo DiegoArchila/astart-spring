01e7081e50f4c70f972e099d091b95a89d094c0c
Diego Alonso Archila <daat3523@gmail.com>
2023-02-04 17:43:16 -0500

    [CREATE] First commit

---
+++ src/main/java/com/app/astart/AstartApplication.java
@@ -0,0 +1,13 @@
+package com.app.astart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/astart/AstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.astart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
